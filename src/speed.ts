e05e91b82f7ad1ef977f2f39dc4e5ce5b91b4e26
Jan Stolle <janstolle@me.com>
2023-03-03 20:04:02 +0100

    Add Scriptable Types

--- src/speed.ts
+++ src/speed.ts
@@ -6,34 +6,34 @@ loadStatus().then(async status => {
   console.log(status)
   let speed = status.speed || 'n/a'
   if (config.runsInWidget) {
-      // Tell the widget on the Home Screen to show our ListWidget instance.
-      let widget = await createSpeedWidget(speed)
-      Script.setWidget(widget)
-    } else if (config.runsWithSiri) {
-      await QuickLook.present(speed)
-    } else {
-      await QuickLook.present(speed)
-    }
+    // Tell the widget on the Home Screen to show our ListWidget instance.
+    let widget = await createSpeedWidget(speed)
+    Script.setWidget(widget)
+  } else if (config.runsWithSiri) {
+    await QuickLook.present(speed)
+  } else {
+    await QuickLook.present(speed)
+  }
 }).finally(() => {
   Script.complete()
 })
 
 async function createSpeedWidget(speed) {
-let widget = new ListWidget()
-widget.backgroundColor = new Color("#EC0016")
-widget.addSpacer()
-let speedTextElement = widget.addText(speed + " km/h")
-speedTextElement.font = Font.boldSystemFont(24)
-speedTextElement.textColor = Color.white()
-speedTextElement.minimumScaleFactor = 0.75
+  let widget = new ListWidget()
+  widget.backgroundColor = new Color("#EC0016")
+  widget.addSpacer()
+  let speedTextElement = widget.addText(speed + " km/h")
+  speedTextElement.font = Font.boldSystemFont(24)
+  speedTextElement.textColor = Color.white()
+  speedTextElement.minimumScaleFactor = 0.75
 
-return widget
+  return widget
 }
 
 //----------------------------------------------------
 
 async function loadStatus() {
-let url = "https://iceportal.de/api1/rs/status"
-let req = new Request(url)
-return await req.loadJSON()
+  let url = "https://iceportal.de/api1/rs/status"
+  let req = new Request(url)
+  return await req.loadJSON()
 }
